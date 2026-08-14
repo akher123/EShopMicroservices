e18bbbb1a4b8f5d1fc5d09f830ea9034c2269f9e
AU <au@nineyard.com>
2026-08-14 12:14:23 +0600

    021 Develop POST Endpoint with Carter implements ICarterModule for Minimal Apis - 2

---
+++ src/Services/Catalog/Catalog.Api/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.Api.Models;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public List<string> Catagory { get;set;  }
+    public string Description { get; set; }
+    public string ImageFile { get; set; }
+    public decimal Price { get; set; }
+}
---
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,33 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.Api.Models;
+
+namespace Catalog.Api.Products.CreateProduct;
+
+public record CreateProductCommand(string Name,
+    List<string> Catagory,
+    string Description,
+    string ImageFile,
+    decimal Price)
+    : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        // create product entity from command object
+    
+        var product = new Product
+        {
+            Name = command.Name,
+            Catagory = command.Catagory,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+        // Save to database
+        // return CreateProductResult result
+        return new CreateProductResult(Guid.NewGuid());
+    }
+}
