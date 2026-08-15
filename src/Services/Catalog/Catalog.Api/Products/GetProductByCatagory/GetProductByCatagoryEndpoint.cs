fe000cacc670fdf6bf61bf40be988280e32f26c6
AU <au@nineyard.com>
2026-08-15 17:14:19 +0600

    019 Develop GetProductByCategory GET Endpoint with Minimal Apis and Carter

---
+++ src/Services/Catalog/Catalog.Api/Products/GetProductByCatagory/GetProductByCatagoryEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.Api.Products.GetProducts;
+
+namespace Catalog.Api.Products.GetProductByCatagory;
+
+public record GetProductByCatagoryResponse(IEnumerable<Product> Products);
+
+public class GetProductByCatagoryEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("products/catagory/{catagory}", async (string catagory, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByCatagoryQuery(catagory));
+            var response = result.Adapt<GetProductByCatagoryResponse>();
+            return Results.Ok(response);
+
+        })
+         .WithName("GetProductByCatagory")
+         .Produces<GetProuctsResponse>(StatusCodes.Status200OK)
+         .ProducesProblem(StatusCodes.Status400BadRequest)
+         .WithSummary("Get Product By Catagory")
+         .WithDescription("Get Product By Catagory");
+    }
+}
---
+++ src/Services/Catalog/Catalog.Api/Products/GetProductByCatagory/GetProductByCatagoryHandler.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Catalog.Api.Products.GetProductByCatagory;
+
+public record GetProductByCatagoryQuery(string Catagory) : IQuery<GetProductByCatagoryResult>;
+public record GetProductByCatagoryResult(IEnumerable<Product> Products);
+public class GetProductByCatagoryQueryHandler(IDocumentSession session, ILogger<GetProductByCatagoryQueryHandler> logger) : IQueryHandler<GetProductByCatagoryQuery, GetProductByCatagoryResult>
+{
+    public async Task<GetProductByCatagoryResult> Handle(GetProductByCatagoryQuery query, CancellationToken cancellationToken)
+    {
+        var products = await session.Query<Product>()
+             .Where(x => x.Catagory.Contains(query.Catagory))
+             .ToListAsync();
+
+        return new GetProductByCatagoryResult(products);
+    }
+}
