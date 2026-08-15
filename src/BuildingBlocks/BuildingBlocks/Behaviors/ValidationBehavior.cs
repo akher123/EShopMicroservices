7f8f70ea47229da8f9fd2a037a22f25b4332d2f3
AU <au@nineyard.com>
2026-08-15 21:05:33 +0600

    005 Validation Pipeline Behaviour with MediatR using Fluent Validation Library

---
+++ src/BuildingBlocks/BuildingBlocks/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,28 @@
+﻿using BuildingBlocks.CQRS;
+using FluentValidation;
+using MediatR;
+
+namespace BuildingBlocks.Behaviors;
+
+public class ValidationBehavior<IRequest, TResponse>
+    (IEnumerable<IValidator<IRequest>> validators)
+    : IPipelineBehavior<IRequest, TResponse>
+    where IRequest:ICommand<TResponse>
+{
+    public async Task<TResponse> Handle(IRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var context = new ValidationContext<IRequest>(request);
+        var validationResults = 
+            await Task.WhenAll(validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+        var failuers= validationResults
+                   .Where(r=>r.Errors.Any())
+                   .SelectMany(r=>r.Errors)
+                   .ToList();
+        if (failuers.Any())
+        {
+            throw new ValidationException(failuers);
+        }
+        return await next();
+    }
+}
--- src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace Catalog.Api.Products.CreateProduct;
+﻿using FluentValidation;
+
+namespace Catalog.Api.Products.CreateProduct;
 
 public record CreateProductCommand(string Name,
     List<string> Catagory,
@@ -9,10 +11,23 @@ public record CreateProductCommand(string Name,
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+        RuleFor(x => x.Catagory).NotEmpty().WithMessage("Catagory is required");
+        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("IamgeFile is required");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+    }
+}
+
+internal class CreateProductCommandHandler(IDocumentSession session,ILogger<CreateProductCommandHandler>logger ) : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
+        logger.LogInformation("CreateProductCommandHandler.Handle called with {@Command}", command);
+
         var product = new Product
         {
             Name = command.Name,
