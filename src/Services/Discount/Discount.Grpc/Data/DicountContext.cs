5194db714bdaf037306ac1a0f3c01e052f621c34
AU <au@nineyard.com>
2026-08-18 15:32:49 +0600

    CRUD  Discount.Grpc Microservices for Microservices Grpc Communication

---
+++ src/Services/Discount/Discount.Grpc/Data/DicountContext.cs
@@ -0,0 +1,19 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public class DicountContext:DbContext 
+    {
+        public DbSet<Coupon> Coupons { get; set; }
+        public DicountContext(DbContextOptions<DicountContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coupon>().HasData(
+                  new Coupon { Id = 1, ProductName = "IPhone X", Description = "IPhone Discount", Amount = 150 },
+                  new Coupon { Id = 2, ProductName = "Samsung 10", Description = "Samsung Discount", Amount = 100 }
+                  );
+        }
+
+    }
+}
